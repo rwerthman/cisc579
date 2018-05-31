08eeb72c7d5e4c30cb23bd7b7ae9eecb0739c423
Bobby <bobby@Bobbys-MBP.hsd1.mi.comcast.net>
2018-05-30 21:28:05 -0400

    Now work on A*

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,8 +1,10 @@
+import copy
 
 def expandElementsChildren(queueElement):
     '''
     Return the children of a state of the list string
     '''
+    queueElement = list(queueElement)
     elementsChildren = []
     for index, character in enumerate(queueElement):
         # Create a copy of the queue element
@@ -18,22 +20,29 @@ def expandElementsChildren(queueElement):
             
             # if there is an empty adjacent space move there
             elif queueElement[index + 1] is '-':
-                # Swap index + 1 with index
+                # Swap the letter with the adjacent space
                 copyQueueElement[index + 1] = copyQueueElement[index]
                 copyQueueElement[index] = '-'
                 # Add the child to the list that will be returned
-                elementsChildren.append(copyQueueElement)
+                elementsChildren.append("".join(copyQueueElement))
                 
-            # if we can jump a letter do that
-            elif index + 2 is not len(queueElement) and queueElement[index + 2] is '-':
-                # Swap index + 2 with index
+            # if we can jump a letter to the right like checkers
+            elif (index + 2) is not len(queueElement) and queueElement[index + 2] is '-':
+                # Jump the letter to the right of the current letter
                 copyQueueElement[index + 2] = copyQueueElement[index]
                 copyQueueElement[index] = '-'
                 # Add the child to the list that will be returned
-                elementsChildren.append(copyQueueElement)
+                elementsChildren.append("".join(copyQueueElement))
                      
     return elementsChildren
- 
+
+def removeDuplicates(queueOfPartialPaths):
+    queueOfPartialPathsWithoutDuplicates = []
+    for path in queueOfPartialPaths:
+        if path not in queueOfPartialPathsWithoutDuplicates:
+            queueOfPartialPathsWithoutDuplicates.append(path)
+    return queueOfPartialPathsWithoutDuplicates
+    
 
 '''
 A*
@@ -50,8 +59,41 @@ If goal is attained then announce success
 '''
 def solveWithAStar(initialState, finalState):
     if len(initialState) < 3:
-        return None
-    return []
+        #return None
+        return False
+    # Add root to queue of partial paths
+    queueOfPartialPaths = [[initialState]]
+    
+    # Until the queue is empty or goal is attained
+    while len(queueOfPartialPaths) > 0:
+        # If first queue element equals the goal
+        if queueOfPartialPaths[0][0] == finalState:
+            # Return the path from the initial state to the final state
+            result = queueOfPartialPaths[0]
+            # Reverse the path before returning it so that it goes 
+            # initial state -> final state
+            # return result[::-1]
+            return True
+        else:
+            # Remove the first queue element
+            firstList = queueOfPartialPaths.pop(0)
+            firstElement = firstList[0]
+            
+            # Adds its children to the front of the queue of the partial paths
+            firstElementsChildren = expandElementsChildren(firstElement)
+            for child in firstElementsChildren:
+                copyOfFirstList = copy.deepcopy(firstList)
+                copyOfFirstList.insert(0, child)
+                queueOfPartialPaths.insert(0, copyOfFirstList)
+            
+            #sort the queue of partial paths by distance traveled plus the estimate of distance to goal
+            queueOfPartialPaths.sort(key=len)
+            
+            # remove redundant paths
+            queueOfPartialPaths = removeDuplicates(queueOfPartialPaths)             
+            
+    # return None
+    return False
 
 '''
 Branch and Bound
@@ -70,27 +112,26 @@ def solveWithBranchAndBound(initialState, finalState):
         return None
     
     # Add root to queue of partial paths
-    # Queue of partial paths is a list of lists
     queueOfPartialPaths = [[initialState]]
     
     # Until the queue is empty or goal is attained
-    while len(queueOfPartialPaths) != 0:
+    while len(queueOfPartialPaths) > 0:
         # If first queue element equals the goal
         if queueOfPartialPaths[0][0] == finalState:
-            # Return the path to the from the initial state
-            # to the final state
+            # Return the path from the initial state to the final state
             result = queueOfPartialPaths[0]
-            # Reverse it before returning it
+            # Reverse the path before returning it so that it goes 
+            # initial state -> final state
             return result[::-1]
         else:
             # Remove the first queue element
-            firstElement = queueOfPartialPaths[0][0]
+            firstList = queueOfPartialPaths.pop(0)
+            firstElement = firstList[0]
             
             # Adds its children to the front of the queue of the partial paths
             firstElementsChildren = expandElementsChildren(firstElement)
-            firstList = queueOfPartialPaths.pop(0)
             for child in firstElementsChildren:
-                copyOfFirstList = firstList[:]
+                copyOfFirstList = copy.deepcopy(firstList)
                 copyOfFirstList.insert(0, child)
                 queueOfPartialPaths.insert(0, copyOfFirstList)
             
--- assignment2/assignment2_test.py
+++ assignment2/assignment2_test.py
@@ -3,55 +3,65 @@ import unittest
 from assignment2 import solveWithAStar
 from assignment2 import solveWithBranchAndBound
 from assignment2 import expandElementsChildren
+from assignment2 import removeDuplicates
 
 class Test(unittest.TestCase):
-
-
-    def testAStarART(self):
-        pass
     
     def testAStarNO(self):
-        result = solveWithAStar(['N', 'O', '-', '-'], ['O', 'N', '-', '-'])
-        self.assertIsNone(result)
-    
+        result = solveWithAStar('no--', 'on--')
+        #self.assertIsNone(result)
+        self.assertFalse(result)
+     
     def testAStarTooSmallLength(self):
-        result = solveWithAStar(['N', 'O'], ['O', 'N'])
-        self.assertIsNone(result)
+        result = solveWithAStar('no', 'on')
+        #self.assertIsNone(result)
+        self.assertFalse(result)
+    
+    def testAStarART(self):
+        # result = solveWithAStar(['A', 'R', 'T', '-', '-', '-'], ['-', '-', '-', 'T', 'A', 'R'])
+        result = solveWithAStar('ART---', '---TAR')
+        self.assertTrue(result)
+        
+        result = solveWithAStar('cooper---', 'C---pooer')
+        self.assertTrue(result)
+        #self.assertEqual(result[-1], ['-', '-', '-', 'T', 'A', 'R'])
     
     def testBranchAndBoundART(self):
-        result = solveWithBranchAndBound(['A', 'R', 'T', '-', '-', '-'], ['-', '-', '-', 'T', 'A', 'R'])
-        self.assertEqual(result,
-                         [
-                             ['A', 'R', 'T', '-', '-', '-'], 
-                             ['A', 'R', '-', 'T', '-', '-'], 
-                             ['A', '-', 'R', 'T', '-', '-'], 
-                             ['A', '-', '-', 'T', 'R', '-'], 
-                             ['-', 'A', '-', 'T', 'R', '-'], 
-                             ['-', 'A', '-', 'T', '-', 'R'], 
-                             ['-', '-', 'A', 'T', '-', 'R'], 
-                             ['-', '-', '-', 'T', 'A', 'R']
-                        ])
-    
+        result = solveWithBranchAndBound('ART---', '---TAR')
+        self.assertTrue(result)
+#         self.assertEqual(result,
+#                          [
+#                              ['A', 'R', 'T', '-', '-', '-'], 
+#                              ['A', 'R', '-', 'T', '-', '-'], 
+#                              ['A', '-', 'R', 'T', '-', '-'], 
+#                              ['A', '-', '-', 'T', 'R', '-'], 
+#                              ['-', 'A', '-', 'T', 'R', '-'], 
+#                              ['-', 'A', '-', 'T', '-', 'R'], 
+#                              ['-', '-', 'A', 'T', '-', 'R'], 
+#                              ['-', '-', '-', 'T', 'A', 'R']
+#                         ])
+     
     def testBranchAndBoundNO(self):
-        result = solveWithBranchAndBound(['N', 'O', '-', '-'], ['O', 'N', '-', '-'])
-        self.assertIsNone(result)
-        
+        result = solveWithBranchAndBound('no--', 'on--')
+        self.assertFalse(result)
+        # self.assertIsNone(result)
+         
     def testBranchAndBoundTooSmallLength(self):
-        result = solveWithBranchAndBound(['N', 'O'], ['O', 'N'])
-        self.assertIsNone(result)
-        
+        result = solveWithBranchAndBound('no', 'on')
+        # self.assertIsNone(result)
+        self.assertFalse(result)
+         
     def testexpandElementsChildren(self):
-        result = expandElementsChildren(['A', 'R', 'T', '-', '-', '-'])
+        result = expandElementsChildren('art---')
         self.assertListEqual(result, 
         [
-            ['A', '-', 'T', 'R', '-', '-'],
-            ['A', 'R', '-', 'T', '-', '-'] 
+            'a-tr--',
+            'ar-t--' 
         ])
-
-    def testBranchAndBoundCooper(self):
-        result = solveWithBranchAndBound(['w', 'e', 'r', '-', '-', '-'], ['w', '-', '-', '-', 'e', 'r'])
-        print result
-        
+    
+    def testRemoveDuplicates(self):
+        result = removeDuplicates([['art--', 'a-tr--'], ['art--', 'a-tr-t'], ['art--', 'a-tr--']])
+        self.assertListEqual(result, [['art--', 'a-tr--'], ['art--', 'a-tr-t']])
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
